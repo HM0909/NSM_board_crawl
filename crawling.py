8c26158998b01979d9aa9c6ecaf1ea521dc0fda8
DESKTOP-LP1VBTM\이혜민 <haemin9209@gmail.com>
2022-09-19 00:58:38 +0900

    로그인 및 세부 크롤링 1차

---
+++ crawling.py
@@ -0,0 +1,95 @@
+from unicodedata import category
+import re
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+import urllib.request as ur
+import time
+from webdriver_manager.chrome import ChromeDriverManager    # Mac
+
+# 국립중앙과학관(NSM)
+
+base_url = "https://www.science.go.kr/mps"
+board_url = "https://www.science.go.kr/board?menuId=MENU00401&siteId=" 
+login_url = "https://rsvn.science.go.kr/nsm/member/login?returnURL= + ?? " 
+
+driver = webdriver.Chrome(executable_path='C:\hm_py\chromedriver')    # Windows
+
+
+def login():
+    driver.get(base_url)
+
+    # 로그인 페이지 이동
+    element = driver.find_element_by_xpath('//*[@id="header"]/div/div/nav/ul/li[2]/a')  
+    driver.execute_script("arguments[0].click();", element)
+    time.sleep(1)
+    driver.switch_to.window(driver.window_handles[-1])  #새로 연 탭으로 이동
+                  
+    # 로그인
+    user_id = ""
+    password = ""
+
+    driver.find_element_by_id('id').send_keys(user_id)
+    time.sleep(5)
+    driver.find_element_by_id('password').send_keys(password)
+    time.sleep(5)
+    element = driver.find_element_by_xpath('//*[@id="loginForm"]/div/ul[2]/li/a') 
+    driver.execute_script("arguments[0].click();", element)                      
+
+    time.sleep(5)
+ 
+
+def crawling():
+    driver.get(board_url)
+    html = driver.page_source
+    soup = bs(html, 'html.parser')
+
+    board_main = soup.find("table",  {"class" : "tstyle-list"}) 
+    board_list = board_main.find_all("tr")
+
+    for item in board_list:    
+        board_number = item.select_one("td")                              # 자료 번호
+        board_number = re.sub('<.+?>', '', str(board_number)).strip()            
+                
+        print(board_number)
+
+        data = board_main.find("td", {"class":"subject"})                                      
+        link = data.find("a")
+        link_url = link.get("href")                                        # 상세 URL
+        
+        print("https://www.science.go.kr"+link_url)                                                          
+        detail("https://www.science.go.kr"+link_url)
+
+#상세 크롤링
+def detail(detail_url):
+    driver.get(detail_url)
+    
+    detail_html = driver.page_source 
+    detail_soup = bs(detail_html, 'html.parser')
+
+    view_header = detail_soup.find("div", {"id" : "detail-contents"})
+    title = view_header.find("h1", {"class" : "view-title"}).text            # 제목
+    
+   
+    header_all = detail_soup.find("ul", {"class" : "info"})
+    reg_date = header_all.select('span')[0].text                             # 등록일
+    read_count = header_all.select('span')[1].text                           # 조회수 
+            
+    
+    content = view_header.find("div", {"class" : "view-content"}).text       # 내용
+    
+                                                  # 첨부파일 URL
+            
+    print(title, reg_date, read_count, content)
+
+def main():
+    
+    # login()
+         
+    crawling()
+    
+    detail('detail_url')
+    
+    driver.quit()
+         
+if __name__ == '__main__':
+    main()
